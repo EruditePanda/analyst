101cc1e71004fac51e5cf9c5e7cc18c0440d96f8
abtv <andreybutov@mail.ru>
2017-03-19 15:13:35 +0300

    deploy to real server

--- ansible/files/elasticsearch_indices.sh
+++ ansible/files/elasticsearch_indices.sh
@@ -1,2 +1,2 @@
-http put http://localhost:9200/tweets < tweets_index.json
-http put http://localhost:9200/tweets/_mapping/tweet < tweet_mapping.json
+http put http://localhost:9200/tweets < /tmp/tweets_index.json
+http put http://localhost:9200/tweets/_mapping/tweet < /tmp/tweet_mapping.json
