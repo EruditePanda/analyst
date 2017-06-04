4071c04c532ef546fa1ec3412e37c655df3b492f
abtv <andreybutov@mail.ru>
2017-06-04 20:21:22 +0300

    news daily mapping in elastic

--- ansible/files/elasticsearch_indices.sh
+++ ansible/files/elasticsearch_indices.sh
@@ -1,2 +1,4 @@
 http put http://localhost:9200/tweets < /tmp/tweets_index.json
 http put http://localhost:9200/tweets/_mapping/tweet < /tmp/tweet_mapping.json
+http put http://localhost:9200/news < /tmp/news_index.json
+http put http://localhost:9200/news/_mapping/daily < /tmp/news_daily_mapping.json
